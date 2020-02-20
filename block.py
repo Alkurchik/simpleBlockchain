8c99207071e810cbce9ccbee6eab0b559b1dd71f
kurchik.sasha@mail.ru <yntahceP123>
2020-02-20 12:36:47 +0300

    refactoring code and print results on terminal

--- block.py
+++ block.py
@@ -2,6 +2,8 @@ import json
 import os
 import hashlib
 
+blocks_dir = os.curdir + '/doc/'
+
 
 def get_hash(file_name):
     blocks_dir = os.curdir + '/doc/'
@@ -9,18 +11,20 @@ def get_hash(file_name):
     return hashlib.md5(file).hexdigest()
 
 
-def check_integrity():
-    blocks_dir = os.curdir + '/doc/'
+def get_files():
     files = os.listdir(blocks_dir)
     files.remove('.DS_Store')
-    files = sorted([int(i) for i in files])
+    return sorted([int(i) for i in files])
 
+
+def check_integrity():
+    files = get_files()
+    results = []
     for file in files[1:]:
         f = open(blocks_dir + str(file))
         h = json.load(f)['hash']
 
         prev_file = str(file - 1)
-
         actual_hash = get_hash(prev_file)
 
         if h == actual_hash:
@@ -28,22 +32,17 @@ def check_integrity():
         else:
             res = "Corrupted"
 
-        print('Block {} is: {}'.format(prev_file, res))
+        # print('Block {} is: {}'.format(prev_file, res))
+        results.append({'block': prev_file, 'result': res})
+
+    return results
 
 
 def write_block(name, amount, to_whom, prev_hash=''):
-
-    blocks_dir = os.curdir + '/doc/'
-
-    files = os.listdir(blocks_dir)
-    files.remove('.DS_Store')
-    files = sorted([int(i) for i in files])
-
-    last_file = files[-1]
-    file_name = str(last_file + 1)
-
-    prev_hash = get_hash(str(last_file))
-
+    files = get_files()
+    prev_file = files[-1]
+    file_name = str(prev_file + 1)
+    prev_hash = get_hash(str(prev_file))
     data = {
         "name"   : name,
         "amount" : amount,
@@ -57,7 +56,8 @@ def write_block(name, amount, to_whom, prev_hash=''):
 
 def main():
     # write_block(name='ivan', amount=2, to_whom='Katya')
-    check_integrity()
+    print(check_integrity())
+
 
 if __name__ == '__main__':
     main()
